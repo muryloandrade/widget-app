f62d6fa0f4831373259a70ba9f23b8acef640600
muryloandrades <muryloandrades@gmail.com>
2023-03-06 17:36:52 -0300

    Widget Structure

--- src/Components/LayoutWidget/WidgetLayout.tsx
+++ src/Components/LayoutWidget/WidgetLayout.tsx
@@ -1,6 +1,7 @@
 import GridLayout from 'react-grid-layout'
 import { IWidget } from '../../interface/IWidget'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 
 export const WidgetLayout = () => {
@@ -9,57 +10,14 @@ export const WidgetLayout = () => {
         setLayout(layout)
       }
 
-    const widgetFake = [
-        {
-            i: 'a',
-            x: 0,
-            y: 0,
-            w: 1,
-            h: 2,
-        },
-        {
-            i: 'b',
-            x: 2,
-            y: 0,
-            w: 1,
-            h: 2,
-        },
-        {
-            i: 'c',
-            x: 4,
-            y: 0,
-            w: 1,
-            h: 2,
-        },
-        {
-            i: 'd',
-            x: 6,
-            y: 0,
-            w: 1,
-            h: 2,
-        },
-        {
-            i: 'e',
-            x: 8,
-            y: 0,
-            w: 1,
-            h: 2,
-        },
-        {
-            i: 'f',
-            x: 10,
-            y: 0,
-            w: 1,
-            h: 2,
-        },
-        {
-            i: 'g',
-            x: 0,
-            y: 2,
-            w: 1,
-            h: 2,
-        },
-    ]
+    useEffect(() => {
+        axios.get('http://localhost:3001/layoutWidget')
+        .then(response => {
+            setLayout(response.data)
+        })
+    }, [])
+
+
     return(
         <GridLayout
               cols={12}
@@ -69,7 +27,7 @@ export const WidgetLayout = () => {
               compactType='horizontal'
               onLayoutChange={onLayoutChange}
               >
-                {widgetFake.map((widget, i) => {
+                {layout.map((widget, i) => {
                     return(
                     <div key={i} data-grid={widget} style={{backgroundColor:"white"}}>
                         <div>
