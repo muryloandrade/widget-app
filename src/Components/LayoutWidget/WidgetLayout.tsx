88de7b290c2974dde1e3ad3d1ef204c0108b3516
muryloandrades <muryloandrades@gmail.com>
2023-03-08 11:28:21 -0300

    start function RTK post

--- src/Components/LayoutWidget/WidgetLayout.tsx
+++ src/Components/LayoutWidget/WidgetLayout.tsx
@@ -1,7 +1,7 @@
 import GridLayout from 'react-grid-layout'
 import { IWidget } from '../../interface/IWidget'
 import { useCallback, useEffect, useState } from 'react'
-import { useGetWidgetsQuery } from '../../services/widget'
+import { useGetWidgetsQuery, usePostWidgetMutation } from '../../services/widget'
 
 
 export const WidgetLayout = () => {
@@ -10,7 +10,7 @@ export const WidgetLayout = () => {
         setLayout(layout)
       }
 
-    // const [testeSave] = usePostWidgetMutation()
+    const [postWidget] = usePostWidgetMutation()
 
     const {data,isSuccess,isLoading,isError} = useGetWidgetsQuery("teste")
 
@@ -21,31 +21,49 @@ export const WidgetLayout = () => {
         }
     }, [data,isSuccess])
 
-    if(isLoading){
-        return <h1>Carregando...</h1>
-    }
-    if(isError){
-        return <h1>Erro</h1>
-    }
 
+    // Daqui até a linha 50 é para adicionar um novo widget
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const addWidget = useCallback(() => {
+        const newWidget = {
+            i: `${layout.length}`,
+            x: 0,
+            y: Infinity,
+            w: 1,
+            h: 1,
+            static: false,
+            moved: false
+        }
+        return newWidget
+    },[layout])
 
+    // Daqui até a linha 70 é para adicionar um novo widget
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const addWidgetToLayout = useCallback(() => {
+        const newWidget = addWidget()
+        setLayout([...layout,newWidget])
+        postWidget(layout)
+    },[addWidget,layout,postWidget])
     
     return(
-      <div>
-        <GridLayout
-              cols={12}
-              rowHeight={30}
-              width={1800}
-              layout={layout}
-              compactType='horizontal'
-              onLayoutChange={onLayoutChange}
-              >
-            {layout.map((item) => (
-                <div key={item.i} data-grid={item} style={{backgroundColor:"white"}}>
-                    <h1>Widget {item.i}</h1>
-                </div>
-            ))}
-        </GridLayout>
+    <div>
+        <div>
+        <button onClick={() => addWidgetToLayout()}>Add Widget</button>
+            <GridLayout
+                cols={12}
+                rowHeight={30}
+                width={1800}
+                layout={layout}
+                compactType='horizontal'
+                onLayoutChange={onLayoutChange}
+                >
+                {layout.map((item) => (
+                    <div key={item.i} data-grid={item} style={{backgroundColor:"white"}}>
+                        <h1>Widget {item.i}</h1>
+                    </div>
+                ))}
+            </GridLayout>
+        </div>
     </div>
     )
 }
